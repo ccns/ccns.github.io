f217c17a707ad426937b420fe09bf3361e87c036
nbswords <dessertn626@gmail.com>
2021-01-08 23:03:34 +0800

    add extra hello info

--- js/hello.js
+++ js/hello.js
@@ -2,7 +2,7 @@ function hello() {
   $.ajax({
     url:'https://freegeoip.app/json/',
     success: function(data){
-          var helloString = 'こんぺこ~  \n' + data.ip + ' From:  ' + data.country_code +' ' + data.time_zone + '\nHA↘️HA↗️HA↘️HA↗️';
+          var helloString = 'こんぺこ~  こんぺこ~\n' + data.ip + ' From:  ' + data.country_code +' ' + data.time_zone + '\nHA↘️HA↗️HA↘️HA↗️';
           alert(helloString);
       },
     dataType: 'json'
