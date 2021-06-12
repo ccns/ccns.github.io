b8f0a8dd0d675f436e8fbb1a1010a55bf7baf47f
gholk <gholk@disroot.org>
2021-06-12 14:40:56 +0800

    make smooth scrolling only apply to in-page link
    in original version, all anchor contain `#` will
    be treat as in-page link, and the code scroll-top.js
    make the link broken.
    
    the selector `[href^='#']` select only anchors
    start with `#`, which are in-page anchor.

--- js/scroll-top.js
+++ js/scroll-top.js
@@ -1,6 +1,6 @@
 $(function() {
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  // Add smooth scrolling to all in-page links
+  $("a[href^='#']").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
