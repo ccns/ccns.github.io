c381788d0f95dbfaa78d812b271961744a8aafc6
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2021-10-31 14:21:09 +0800

    refactor(generate.js): deduplicate code
    * Deduplicate page-rendering code with new renderPage()
    * Structure the rendering variables for each page into `pages`
    * Render pages with forEach on `pages`

--- generate.js
+++ generate.js
@@ -4,53 +4,52 @@ const moment = require("moment");
 const request = require("request");
 moment.locale("zh-tw");
 
-const events_url = "https://ccns.kktix.cc/events.json";
+const renderPage = (src, dst, resources) => {
+  ejs.renderFile(src, resources, null, (err, html) => {
+    // console.log(html);
+    fs.writeFileSync(dst, html, "utf8");
+  });
+}
 
 // events
-request.get(events_url, (error, response, body) => {
-  if (!error && response.statusCode == 200) {
-    var events = JSON.parse(body);
-    var events = events.entry.map((e) => {
-      var content = e.content;
-      var spl = content.split("\n");
-      var time_origin = spl[0].split("：")[1].split("~")[0];
-      var place = spl[1].split("：")[1].split("/")[0];
+const events_url = "https://ccns.kktix.cc/events.json";
+const renderEvents = (src, dst) => {
+  request.get(events_url, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      var events = JSON.parse(body);
+      var events = events.entry.map((e) => {
+        var content = e.content;
+        var spl = content.split("\n");
+        var time_origin = spl[0].split("：")[1].split("~")[0];
+        var place = spl[1].split("：")[1].split("/")[0];
 
-      var time = moment(time_origin, "YYYY/MM/DD HH:mm(Z)").format(
-        "YYYY年M月D日 Ah點"
-      );
+        var time = moment(time_origin, "YYYY/MM/DD HH:mm(Z)").format(
+          "YYYY年M月D日 Ah點"
+        );
 
-      e.time = time;
-      e.place = place;
-      return e;
-    });
+        e.time = time;
+        e.place = place;
+        return e;
+      });
+      renderPage(src, dst, { events: events });
+    } else {
+      console.log("Failed to fetch events.");
+    }
+  });
+};
 
-    ejs.renderFile("./events.ejs", { events: events }, null, (err, html) => {
-      // console.log(html);
-      fs.writeFileSync("./events.html", html, "utf8");
-    });
-  } else {
-    console.log("Failed to fetch events.");
-  }
-});
-
-// members
 const members = require("./resources/members.json");
-ejs.renderFile("./members.ejs", { members: members }, null, (err, html) => {
-  // console.log(html);
-  fs.writeFileSync("./members.html", html, "utf8");
-});
-
-// projects
 const projects = require("./resources/projects.json");
-ejs.renderFile("./projects.ejs", { projects: projects }, null, (err, html) => {
-  //console.log(html);
-  fs.writeFileSync("./projects.html", html, "utf8");
-});
-
-// timeline
 const timeline = require("./resources/timeline.json");
-ejs.renderFile("./intro.ejs", { timeline: timeline }, null, (err, html) => {
-  // console.log(html);
-  fs.writeFileSync("./intro.html", html, "utf8");
+const pages = [
+  { src: "./events.ejs", dst: "./events.html", renderer: renderEvents },
+  { src: "./members.ejs", dst: "./members.html", resources: { members: members } },
+  { src: "./projects.ejs", dst: "./projects.html", resources: { projects: projects } },
+  { src: "./intro.ejs", dst: "./intro.html", resources: { timeline: timeline } },
+];
+
+pages.forEach((e, i) => {
+  if (e.renderer && e.renderer.call && e.renderer.apply)
+    return e.renderer(e.src, e.dst);
+  renderPage(e.src, e.dst, e.resources || {});
 });
