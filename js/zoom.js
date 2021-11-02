7412b3770c8cc809561019c8bd44e3fea8962f13
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2021-11-02 11:52:55 +0800

    chore(zoom.js): recalc --scrollbar-width on window resizing

--- js/zoom.js
+++ js/zoom.js
@@ -1,5 +1,4 @@
 $(function() {
-  $(":root").css("--scrollbar-width", ($(window).outerWidth() - $(":root").width()).toString() + "px");
   $(document.body).prepend('<div id="zoom-background"></div>');
   $(".zoom").click(function() {
     var zoomed = $(this).hasClass("zoomed");
@@ -14,4 +13,19 @@ $(function() {
       $(".zoomed").removeClass("zoomed");
     }
   });
+  var pWidth = [0];
+  var pBarWidth = [0];
+  function updateScrollbarW() {
+    var newWidth = $(this).outerWidth();
+    if (newWidth !== pWidth[0]) {
+      var newBarWidth = newWidth - $(":root").width();
+      if (newBarWidth !== pBarWidth[0]) {
+        $(":root").css("--scrollbar-width", newBarWidth.toString() + "px");
+        pBarWidth[0] = newBarWidth;
+      }
+      pWidth[0] = newWidth;
+    }
+  }
+  updateScrollbarW($(window));
+  $(window).on("resize", updateScrollbarW);
 })
